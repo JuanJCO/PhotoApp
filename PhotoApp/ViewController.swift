86b2401673bd611986b6e57b302f2a4c95a6acfd
JuanJCO <jcarroolmedo@gmail.com>
2021-03-11 02:05:53 +0100

    Initial Commit

--- PhotoApp/ViewController.swift
+++ PhotoApp/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import SwiftyAvatar
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    let imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var avatar: SwiftyAvatar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
     }
-
-
+    
+    @IBAction func chooseImage(_ sender: UIButton) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        avatar.image = image
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        imagePicker.dismiss(animated: true, completion: nil)
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
 
